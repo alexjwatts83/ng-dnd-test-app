293493da834c5d7b392927c5e825ac0e04905737
Alex Watts <awatts@kitchen-net.com.au>
2018-07-05 22:18:21 +1000

    wip commit

--- ng-dnd-test-app/src/app/models/dnd-time-slot.ts
+++ ng-dnd-test-app/src/app/models/dnd-time-slot.ts
@@ -1,15 +1,11 @@
 import { DndCustomer } from './dnd-customer';
-export interface IDndPostCodeDisplay {
-    PostCode: string,
-    NumberOfCustomers: number;
-}
 
 export interface IDndTimeSlot {
     Time: string;
     TimeSlotsAllocated: number;
     TimeSlotsTotal: number;
     Customers: DndCustomer[];
-    PostCodeDisplay: IDndPostCodeDisplay[];
+    // PostCodeDisplay: IDndPostCodeDisplay[];
     OpenCustomers: boolean;
 }
 
@@ -21,29 +17,6 @@ export class DndTimeSlot implements IDndTimeSlot {
     
     get TimeSlotsAllocated(): number {
         return this.Customers.length;
-    }
-    get PostCodeDisplay(): IDndPostCodeDisplay[] {
-        let postCodeDisplay: IDndPostCodeDisplay[] = [];
-        this.Customers.forEach(x => {
-            let postCodeInList = false;
-            postCodeDisplay.forEach(y => {
-                if (x.PostCode === y.PostCode) {
-                    postCodeInList = true;
-                    y.NumberOfCustomers = y.NumberOfCustomers + 1;
-                    return;
-                }
-                if (postCodeInList) {
-                    return;
-                }
-            });
-            if (!postCodeInList) {
-                postCodeDisplay.push({
-                    PostCode: x.PostCode,
-                    NumberOfCustomers: 1
-                });
-            }
-        });
-        return postCodeDisplay;
     }
     
     constructor(time: string, timeSlotsTotal: number, customers: DndCustomer[]) {
